a5e2f3355820bedb74c849d5abb38e9b0c63e6b5
Bart Pelle <blackinmen@gmail.com>
2017-06-02 17:30:29 -0400

    Export reference table fields

--- referencetable.go
+++ referencetable.go
@@ -7,9 +7,9 @@ import (
 // ReferenceTable represents a table of data holding specifics about
 // the filesystem index, such as the revision of entries, names and checksums.
 type ReferenceTable struct {
-	version    int8
-	revision   int32
-	entryCount int32
+	Version    int8
+	Revision   int32
+	EntryCount int32
 	ids        []int32
 	size       int32
 	names      []int32
@@ -62,10 +62,10 @@ func DecodeReferenceTable(data []byte) (*ReferenceTable, error) {
 	buffer := ByteBuffer{data: data}
 	table := ReferenceTable{}
 
-	table.version = buffer.int8()
-	if table.version >= 5 && table.version <= 7 {
-		if table.version >= 6 {
-			table.revision = buffer.int32()
+	table.Version = buffer.int8()
+	if table.Version >= 5 && table.Version <= 7 {
+		if table.Version >= 6 {
+			table.Revision = buffer.int32()
 		}
 
 		flags := buffer.int8()
@@ -74,19 +74,19 @@ func DecodeReferenceTable(data []byte) (*ReferenceTable, error) {
 		unknown1 := (flags & 4) != 0
 		unknown2 := (flags & 8) != 0
 
-		if table.version >= 7 {
-			table.entryCount = buffer.varint()
+		if table.Version >= 7 {
+			table.EntryCount = buffer.varint()
 		} else {
-			table.entryCount = int32(buffer.int16()) & 0xFFFF
+			table.EntryCount = int32(buffer.int16()) & 0xFFFF
 		}
-		table.ids = make([]int32, table.entryCount)
+		table.ids = make([]int32, table.EntryCount)
 
 		id := int32(0)
 		max := int32(-1)
 
-		for i := int32(0); i < table.entryCount; i++ {
+		for i := int32(0); i < table.EntryCount; i++ {
 			// Type of data depends on the table version - only 7+ supports >65535
-			if table.version >= 7 {
+			if table.Version >= 7 {
 				id += buffer.varint()
 			} else {
 				id += int32(buffer.int16()) & 0xFFFF
@@ -107,20 +107,20 @@ func DecodeReferenceTable(data []byte) (*ReferenceTable, error) {
 			table.names = make([]int32, table.size)
 
 			// Set all names to -1 (meaning no name)
-			for i := int32(0); i < table.entryCount; i++ {
+			for i := int32(0); i < table.EntryCount; i++ {
 				table.names[table.ids[i]] = buffer.int32()
 			}
 		}
 
 		// Load CRC values
 		table.crc32 = make([]int32, table.size)
-		for i := int32(0); i < table.entryCount; i++ {
+		for i := int32(0); i < table.EntryCount; i++ {
 			table.crc32[table.ids[i]] = buffer.int32()
 		}
 
 		// Unidentified
 		if unknown2 {
-			for i := int32(0); i < table.entryCount; i++ {
+			for i := int32(0); i < table.EntryCount; i++ {
 				buffer.int32()
 			}
 		}
@@ -128,14 +128,14 @@ func DecodeReferenceTable(data []byte) (*ReferenceTable, error) {
 		// Read whirlpool values
 		if hasWhirlpool {
 			table.whirlpool = make([][]byte, table.size)
-			for i := int32(0); i < table.entryCount; i++ {
+			for i := int32(0); i < table.EntryCount; i++ {
 				table.whirlpool[table.ids[i]] = buffer.bytes(64)
 			}
 		}
 
 		// Unidentified
 		if unknown1 {
-			for i := int32(0); i < table.entryCount; i++ {
+			for i := int32(0); i < table.EntryCount; i++ {
 				buffer.int32()
 				buffer.int32()
 			}
@@ -143,13 +143,13 @@ func DecodeReferenceTable(data []byte) (*ReferenceTable, error) {
 
 		// Load entry versions
 		table.versions = make([]int32, table.size)
-		for i := int32(0); i < table.entryCount; i++ {
+		for i := int32(0); i < table.EntryCount; i++ {
 			table.versions[table.ids[i]] = buffer.int32()
 		}
 
 		return &table, nil
 	} else {
-		return nil, fmt.Errorf("invalid reference table version: %d", table.version)
+		return nil, fmt.Errorf("invalid reference table version: %d", table.Version)
 	}
 
 	return nil, nil
